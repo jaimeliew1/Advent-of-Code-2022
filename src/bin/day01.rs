c0ba488461590bbf8ad375b456b3ac8c9b3f5c48
Jaime <jaimeliew1@gmail.com>
2022-12-01 09:21:33 +0100

    add day01

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,13 +1,23 @@
-
-fn parse_input(filename: &str) -> Vec<i32> {
+use itertools::Itertools;
+fn parse_input(filename: &str) -> Vec<Vec<u64>> {
     let contents = std::fs::read_to_string(filename).expect("can't find file");
-    let data: Vec<i32> = contents.lines().map(|s| s.parse().unwrap()).collect();
-    data
+    contents
+        .split("\n\n")
+        .map(|par| par.lines().map(|l| l.parse().unwrap()).collect())
+        .collect()
 }
 
 fn solve() -> (u64, u64) {
     let data = parse_input("input/day01.txt");
-    (0, 0)
+    let top_three: Vec<u64> = data
+        .iter()
+        .map(|chunk| chunk.iter().sum())
+        .sorted()
+        .rev()
+        .take(3)
+        .collect();
+
+    (top_three[0], top_three.iter().sum())
 }
 
 aoc2022::aoc!(solve);
