f5857ac4dc3bd47c9f92f5d622f0cfd2fdf07af4
Jaime <jaimeliew1@gmail.com>
2022-12-06 15:34:51 +0100

    simplify day06

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,23 +1,9 @@
-use std::collections::HashSet;
+use itertools::Itertools;
 
-fn parse_input(filename: &str) -> Vec<char> {
-    std::fs::read_to_string(filename)
-        .expect("can't find file")
-        .chars()
-        .collect()
-}
-
-fn first_all_unique_occurence(data: &[char], n: usize) -> usize {
-    data.windows(n)
-        .map(|x| HashSet::<char>::from_iter(x.to_owned()).len() == n)
-        .position(|x| x)
-        .unwrap()
-        + n
-}
 fn solve() -> (u64, u64) {
-    let data = parse_input("input/day06.txt");
-    let part1 = first_all_unique_occurence(&data, 4);
-    let part2 = first_all_unique_occurence(&data, 14);
+    let data: Vec<char> = std::fs::read_to_string("input/day06.txt").unwrap().chars().collect();
+    let part1 = data.windows(4).position(|x| x.iter().all_unique()).unwrap() + 4;
+    let part2 = data.windows(14).position(|x| x.iter().all_unique()).unwrap() + 14;
 
     (part1 as u64, part2 as u64)
 }
