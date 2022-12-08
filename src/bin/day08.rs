1a6fbbdd6faff4b5c8a3ca0bd8f61933d2ff5898
Jaime <jaimeliew1@gmail.com>
2022-12-08 13:04:53 +0100

    tidy up day08

--- src/bin/day08.rs
+++ src/bin/day08.rs
@@ -11,44 +11,36 @@ fn parse_input(filename: &str) -> Vec<Vec<u64>> {
 fn solve() -> (u64, u64) {
     let data = parse_input("input/day08.txt");
 
-    let mut edge_view: Vec<Vec<bool>> = data
-        .iter()
-        .map(|l| l.iter().map(|_| false).collect())
-        .collect();
-    let mut trees_visible: Vec<Vec<u64>> =
-        data.iter().map(|l| l.iter().map(|_| 1).collect()).collect();
+    let (mut edge_view_count, mut best_score) = (0, 0);
+    for (i, j) in (0..data.len()).cartesian_product(0..data[0].len()) {
+        let height = data[i][j];
+        let (mut edge_view, mut score) = (false, 1);
+        for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)].iter() {
+            let treerow: Vec<&u64> = (1..)
+                .map(|c| {
+                    data.get((i as i32 + c * dy) as usize)
+                        .and_then(|row| row.get((j as i32 + c * dx) as usize))
+                })
+                .while_some()
+                .collect();
 
-    for (i, row) in data.iter().enumerate() {
-        for (j, height) in row.iter().enumerate() {
-            for (dx, dy) in [(0, 1), (0, -1), (1, 0), (-1, 0)].iter() {
-                let treerow: Vec<&u64> = (1..)
-                    .map(|c| {
-                        data.get((i as i32 + c * dy) as usize)
-                            .and_then(|row| row.get((j as i32 + c * dx) as usize))
-                    })
-                    .while_some()
-                    .collect();
-                // part 1
-                edge_view[i][j] |= treerow.iter().all(|&&h| h < *height);
-                // part 2
-                let mut score = 0;
-                for h in treerow.iter() {
-                    if *h < height {
-                        score += 1
-                    } else {
-                        score += 1;
-                        break;
-                    }
+            edge_view |= treerow.iter().all(|&&h| h < height);
+            let mut s = 0;
+            for h in treerow.iter() {
+                if **h < height {
+                    s += 1
+                } else {
+                    s += 1;
+                    break;
                 }
-                trees_visible[i][j] *= score;
             }
+            score *= s;
         }
+        best_score = best_score.max(score);
+        edge_view_count += edge_view as u64;
     }
 
-    (
-        edge_view.iter().flatten().filter(|&&x| x).count() as u64,
-        *trees_visible.iter().flatten().max().unwrap(),
-    )
+    (edge_view_count, best_score)
 }
 
 aoc2022::aoc!(solve);
